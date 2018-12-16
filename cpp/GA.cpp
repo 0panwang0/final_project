c94337882c94fb153596aa5bc281c5e08973a84a
James Brown <wck593462925@outlook.com>
2018-12-16 17:55:20 +0800

    Add some simple comments.

--- cpp/GA.cpp
+++ cpp/GA.cpp
@@ -3,16 +3,20 @@
 
 using namespace std;
 
-std::default_random_engine generator;
+default_random_engine generator;
 
+// 比较两条染色体适应度，升序排序
 int cmp(const ChrFit& chf1, const ChrFit& chf2){
     return chf1.fitness < chf2.fitness;
 }
 
+// 产生一条染色体，通过初始值突变完成
 uint32_t generateChromosome() {
-    return generator() % UINT32_MAX;
+    uint32_t chr = 274922754;
+    return chromosomeMutation(chr);
 }
 
+// 打印单条染色体上的基因
 void printChromosome(uint32_t chr) {
     unsigned char* temp = (unsigned char*)&chr;
     for(int i = 0; i < 4; i++){
@@ -26,6 +30,7 @@ void printChromosome(uint32_t chr) {
     printf("]\n");
 }
 
+// 染色体交叉，一条保留前面k个基因，另一条保留后面4-k个基因
 uint32_t chromosomeCrossover(uint32_t chr1, uint32_t chr2) {
     uint32_t temp = generator() % 4;
     temp = (1 << (temp * 8))-1;
@@ -34,6 +39,7 @@ uint32_t chromosomeCrossover(uint32_t chr1, uint32_t chr2) {
     return (chr1 & temp) | (chr2 & (UINT32_MAX-temp));
 }
 
+// 染色体变异，单个基因的某一位翻转
 uint32_t chromosomeMutation(uint32_t chr) {
     uint32_t temp = generator() % 32;
     temp = 1 << temp;
@@ -42,6 +48,7 @@ uint32_t chromosomeMutation(uint32_t chr) {
     return chr ^ temp;
 }
 
+// 使用两条染色体初始化权重，进行黑白棋对抗
 int chromosomeCompetition(Reversi& game, AI& bot, uint32_t chr1, uint32_t chr2) {
     int idx;
     bool round;
@@ -64,6 +71,7 @@ int chromosomeCompetition(Reversi& game, AI& bot, uint32_t chr1, uint32_t chr2) 
     return (!round) ^ (game.Evaluate() > 0);
 }
 
+// 产生一个特定大小的族群
 uint32_t* GA::generateGroup(int size){
     uint32_t* group = new uint32_t[size];
     for(int i = 0; i < size; i++){
@@ -72,10 +80,12 @@ uint32_t* GA::generateGroup(int size){
     return group;
 }
 
+// 销毁一个特定大小的族群
 void GA::destroyGroup(uint32_t *group) {
     delete[] group;
 }
 
+// 计算一个族群中每个个体的适应度，通过类似淘汰赛的机制累加积分得到适应度
 ChrFit* GA::calculateFitness(uint32_t *group, int size) {
     printf("calculateFitness\n");
     ChrFit* result = new ChrFit[size];
@@ -118,6 +128,7 @@ ChrFit* GA::calculateFitness(uint32_t *group, int size) {
     return result;
 }
 
+// 族群自然选择，将族群中适应度最高的部分族群放到族群后部，以便保留
 void GA::doReproduction(ChrFit *groupFit, uint32_t* group, int size, int chosen) {
     printf("doReproduction\n");
     for(int i = size-1; i >= size - chosen; i--){
@@ -125,6 +136,7 @@ void GA::doReproduction(ChrFit *groupFit, uint32_t* group, int size, int chosen)
     }
 }
 
+// 用族群中其他染色体交叉得到的新染色体代替未在自然选择中被保留的染色体
 void GA::doCrossover(ChrFit *groupFit, uint32_t *group, int size) {
     printf("doCrossover\n");
     int index;
@@ -140,6 +152,7 @@ void GA::doCrossover(ChrFit *groupFit, uint32_t *group, int size) {
     delete[] groupFit;
 }
 
+// 对交叉产生的新染色体进行变异
 void GA::doMutation(uint32_t *group, int size) {
     printf("doMutation\n");
     for(int i = 0; i < size; i++){
@@ -153,17 +166,17 @@ void GA::algorithm() {
 
     for(int i = 0; i < epoch; i++){
         printf("Epoch:\t%d\n", i);
-        ChrFit* grpFit = calculateFitness(group, size);
-        loss = grpFit[size-1].fitness - grpFit[0].fitness;
+        ChrFit* grpFit = calculateFitness(group, size);     // 计算适应度
+        loss = grpFit[size-1].fitness - grpFit[0].fitness;  // 这个Loss不太靠谱，看看就好
         printf("Loss:\t%d\n", loss);
 //        if(loss < 5){
 //            break;
 //        }
-        doReproduction(grpFit, group, size, chosen);
-        doCrossover(grpFit, group, size - chosen);
-        doMutation(group, size - chosen);
-        printChromosome(group[size-1]);
-        printChromosome(group[0]);
+        doReproduction(grpFit, group, size, chosen);        // 自然选择
+        doCrossover(grpFit, group, size - chosen);          // 交叉繁衍
+        doMutation(group, size - chosen);                   // 基因变异
+        printChromosome(group[size-1]);                     // 处于优势的染色体
+        printChromosome(group[0]);                          // 处于劣势的染色体
     }
     destroyGroup(group);
 }
